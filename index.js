2bc1e096b36e46a0f966ebc4e06f952458585e49
nadiia.kamenska <nadiia.kamenska@finiata.com>
2019-11-07 18:06:25 +0100

    added posibility to create an object from file

--- index.js
+++ index.js
@@ -4,36 +4,42 @@ const Library = require('./models/library');
 const Database = require('./models/database')
 
 
-max = new Customer("Max", 77777);
-eva = new Customer("Eva", 65437);
+// max = new Customer("Max", 77777);
+// eva = new Customer("Eva", 65437);
 
-book1 = new Book("BigBook", 1);
-book2 = new Book("RedBook", 2);
-book3 = new Book("Первая книга", 3);
+// book1 = new Book("BigBook", 1);
+// book2 = new Book("RedBook", 2);
+// book3 = new Book("Первая книга", 3);
 
+// library1 = new Library("Berlin City Library", "Breite Str. 30-36, 10178 Berlin");
+// library2 = new Library("Philological Library", "Habelschwerdter Allee 45, 14195 Berlin")
 
-library1 = new Library("Berlin City Library", "Breite Str. 30-36, 10178 Berlin");
-library2 = new Library("Philological Library", "Habelschwerdter Allee 45, 14195 Berlin")
+// library1.addBook(book1);
+// library1.addBook(book2);
+// library1.addBook(book3);
 
-library1.addBook(book1);
-library1.addBook(book2);
-library1.addBook(book3);
+// console.log(library1.listBooks)
+// max.assignToLibrary(library1);
 
-library1.listBooks();
-//console.log(library1.listBooks)
-max.assignToLibrary(library1);
-
-eva.takeBook(book1, "13.11.2018");
-eva.takeBook(book2, "13.11.2018");
-max.takeBook(book1, "13.11.2018");
+//  eva.takeBook(book3, "13.11.2018");
+//  eva.takeBook(book2, "13.11.2018");
+//  max.takeBook(book1, "13.11.2018");
 
 //console.log(book1.owner);
 
-Database.save('customer.json', max)
-Database.save('book.json', book1)
-Database.save('library.json', library1)
-loadedFile = Database.load('library.json')
-console.log(loadedFile)
+// Database.save('customer.json', eva);
+// Database.save('book.json', book1);
+// Database.save('library.json', library1);
 
+const loadedFile = Database.load('library.json');
 
+//const library1 = new Library(loadedFile.name, loadedFile.address, loadedFile.allBooks, loadedFile.allCustomers)
+const library2 = Library.create(loadedFile)
+
+const library3 = new Library("New Library", "Street 1")
+console.log(library.allBooks[0].assignToLibrary(library3));
+
+//library2.listBooks
+//library1.allBooks.forEach(book => console.log(book.name))
+//library2.listCustomers
 
